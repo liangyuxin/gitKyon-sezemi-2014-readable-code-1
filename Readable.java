2edcbe957288222d128c222f5104221510cff1b6
Kyosuke Tomoda <tomoda.kyousuke@ci.i.u-tokyo.ac.jp>
2014-06-22 11:30:38 +0900

    仕様3を訂正

--- Readable.java
+++ Readable.java
@@ -1,13 +1,14 @@
+import java.io.*;
+import java.util.*;
+
 class Readable{
 
     public static void main(String[] args){
         String fileName = args[0];
         try{
             BufferedReader bf = new BufferedReader(new FileReader("fileName"));
-            String line = "";
-            while ((line = bf.readLine()) != null){
-                System.out.println(line);
-            }
+            System.out.println(bf.readLine());
+            bf.close();
         }catch(IOException e){
             System.err.println("READ ERROR: " + fileName);
         }
